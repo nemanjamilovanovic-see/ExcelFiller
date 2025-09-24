a785774ecdc147e6f1b4cf651f1816eddbd54e93
Nemanja Milovanović <nemanja.milovanovic@asseco-see.rs>
2025-09-24 17:08:06 +0200

    chore: prepare v1.0.0 release

--- obj/Debug/net8.0-windows/ExcelFiller.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/ExcelFiller.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExcelFiller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bef2bbda1b4b26dabda9c53442f2f2a646cd203")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca09e28e6648d1f949d50a46b3104628ca288d67")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExcelFiller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExcelFiller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
